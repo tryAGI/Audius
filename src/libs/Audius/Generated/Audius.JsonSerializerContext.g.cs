7edf07b8fc302f9e4ecdc4989bfe1384b953a1e7
Konstantin S. <havendv@gmail.com>
2026-09-04 11:30:21 +0400

    feat: Updated OpenAPI spec (#5)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Audius/Generated/Audius.JsonSerializerContext.g.cs
+++ src/libs/Audius/Generated/Audius.JsonSerializerContext.g.cs
@@ -962,13 +962,8 @@ namespace Audius
             return Resolver.GetTypeInfo(type, Options);
         }
 
-        private static global::System.Text.Json.JsonSerializerOptions CreateDefaultOptions()
+         static void AddConverters(global::System.Text.Json.JsonSerializerOptions options)
         {
-            var options = new global::System.Text.Json.JsonSerializerOptions
-            {
-                DefaultIgnoreCondition = global::System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-                TypeInfoResolver = Resolver,
-            };
             options.Converters.Add(new global::Audius.JsonConverters.AccessGateJsonConverter());
             options.Converters.Add(new global::Audius.JsonConverters.TrackActivityJsonConverter());
             options.Converters.Add(new global::Audius.JsonConverters.CollectionActivityWithoutTracksJsonConverter());
@@ -980,8 +975,17 @@ namespace Audius
             options.Converters.Add(new global::Audius.JsonConverters.OneOfJsonConverter<global::Audius.PlaylistLibraryFolder, global::Audius.PlaylistLibraryPlaylistIdentifier, global::Audius.PlaylistLibraryExplorePlaylistIdentifier>());
             options.Converters.Add(new global::Audius.JsonConverters.OneOfJsonConverter<global::Audius.PlaylistLibraryFolder, global::Audius.PlaylistLibraryPlaylistIdentifier, global::Audius.PlaylistLibraryExplorePlaylistIdentifier>());
             options.Converters.Add(new global::Audius.JsonConverters.UnixTimestampJsonConverter());
-
             options.Converters.Add(new LazyEnumJsonConverterFactory());
+        }
+
+        private static global::System.Text.Json.JsonSerializerOptions CreateDefaultOptions()
+        {
+            var options = new global::System.Text.Json.JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = global::System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+                TypeInfoResolver = Resolver,
+            };
+            AddConverters(options);
 
             return options;
         }
